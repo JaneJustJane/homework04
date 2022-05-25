e5ed5a6a9828545d51666e5b055ecbfb83b9e527
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-25 14:02:40 +0300

    array copy

---
+++ task02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу копирования массива
+void PrintArray (int [] arr)
+{
+    for (int x=0; x<arr.Length; x++)
+    {
+        Console.Write($"{arr[x]} ");
+    }
+}
+
+int [] array = new int [9]{1,4,5,6,2,4,0,3,6};
+Console.WriteLine($"Заданный массив:");
+PrintArray(array);
+Console.WriteLine();
+int [] array1 = new int [9];
+int j=0; 
+
+for (int i=0; i<array.Length; i++)
+{
+        array1[j]=array[i];
+        j++;
+}
+Console.WriteLine($"Копия заданного массива:");
+PrintArray(array1); 
